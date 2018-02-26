cef71bb3656aefce0b4b9a3bf0dbb036f493baae
BuildTools <clement.ababou@outlook.com>
2018-02-26 02:53:33 +0100

    Possible fix

--- listcontainer_addelement.c
+++ listcontainer_addelement.c
@@ -6,7 +6,7 @@
 /*   By: cababou <cababou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 05:13:38 by cababou           #+#    #+#             */
-/*   Updated: 2018/02/17 05:53:25 by cababou          ###   ########.fr       */
+/*   Updated: 2018/02/26 02:53:16 by cababou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_list	*lstcontainer_add(t_lstcontainer *container, void *content, size_t size)
 	if (container->firstelement == NULL)
 		container->firstelement = ft_lstnew(content, size);
 	else
-		ft_lstadd(container->firstelement, ft_lstnew(content, size));
+		ft_lstadd(ft_lstgetlast(container->firstelement),
+				ft_lstnew(content, size));
 	return (ft_lstgetlast(container->firstelement));
 }
